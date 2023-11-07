05cb543348c0c4b2a68ed3b487d1037b63a973e6
StevieBrooks <brookscodeblog@gmail.com>
2023-11-07 14:10:22 +0700

    import audio for practice runs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,64 @@
+// MISC IMPORTS
 import './App.css';
 import { useState, useEffect } from 'react';
+
+
+// COMPONENT IMPORTS
 import Header from './components/Header';
 import TimeScore from './components/TimeScore';
 import GameArea from './components/GameArea';
 import Footer from './components/Footer';
 import GameContext from "./ContextFile"
 
+
+// FILE IMPORTS
 import mole from "./images/mole.png"
 import bird from "./images/bird.png"
 import cat from "./images/cat.png"
 import dog from "./images/dog.png"
 import zebra from "./images/zebra.png"
 
+import moleMP3 from "./audio/mole.mp3"
+import birdMP3 from "./audio/bird.mp3"
+import catMP3 from "./audio/cat.mp3"
+import dogMP3 from "./audio/dog.mp3"
+import zebraMP3 from "./audio/zebra.mp3"
 
+
+// GLOBAL VARIABLES, ARRAYS, ETC
+const holes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+const animalArray = [
+    {icon: mole, name: "mole"},
+    {icon: bird, name: "bird"},
+    {icon: cat, name: "cat"},
+    {icon: dog, name: "dog"},
+    {icon: zebra, name: "zebra"},
+]
+
+const animalAudioArray = [
+    {word: moleMP3, name: "mole"},
+    {word: birdMP3, name: "bird"},
+    {word: catMP3, name: "cat"},
+    {word: dogMP3, name: "dog"},
+    {word: zebraMP3, name: "zebra"},
+]
+
+
+// MAIN
 function App() {
 
-    const holes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     const [holeOccupied, setHoleOccupied] = useState(null)
-
-    const animalArray = [
-        {icon: mole, name: "mole"},
-        {icon: bird, name: "bird"},
-        {icon: cat, name: "cat"},
-        {icon: dog, name: "dog"},
-        {icon: zebra, name: "zebra"},
-    ]
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             const holePop = Math.floor(Math.random() * holes.length);
             setHoleOccupied(holes[holePop]);
-        }, 3000);
+
+            const randomAnimalAudio = animalAudioArray[Math.floor(Math.random() * animalAudioArray.length)].word
+            const audio = new Audio(randomAnimalAudio)
+            // audio.play()
+        }, 2000);
 
         return () => {
             clearInterval(intervalId);
