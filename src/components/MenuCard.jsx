39b4afc40796cf7e7df4ef8d1806bb43a3d7c938
StevieBrooks <brookscodeblog@gmail.com>
2023-11-22 06:27:37 +0700

    build exit menu function

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -5,18 +5,22 @@ export default function MenuCard() {
 
     const [holesOccupied, setHolesOccupied, holes, animalArray, randomAnimalAudio, points, setPoints, menuActive, setMenuActive] = useContext(GameContext)
 
+    const exitMenuFunction = () => {
+        setMenuActive(false)
+    }
+
     return (
         <div className="menu-container">
-            <div className="menu">
+            <div className="menu flex flex-col items-center">
                 <header>
-                    <h2>Game Menu</h2>
-                    <ul>
-                        <li>Instructions</li>
-                        <li>Settings</li>
-                        <li>Scoreboard</li>
-                        <li>Back</li>
-                    </ul>
+                    <h2 className="font-bold">Game Menu</h2>
                 </header>
+                <ul className="mt-5">
+                    <li className="py-2">Instructions</li>
+                    <li className="py-2">Settings</li>
+                    <li className="py-2">Scoreboard</li>
+                    <li className="py-2" onClick={exitMenuFunction}>Exit Menu</li>
+                </ul>
             </div>
         </div>
     )
