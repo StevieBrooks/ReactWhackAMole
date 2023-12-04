75631ff3078aabb3751a50c7bbbe2ee5effbaed7
StevieBrooks <brookscodeblog@gmail.com>
2023-12-04 10:18:41 +0700

    add scores to db server

--- PHP/server.php
+++ PHP/server.php
@@ -1,19 +1,31 @@
-<?php 
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
 
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+include 'database.php';
 
-    include 'database.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the required fields are present
+    if(isset($_POST['difficulty'], $_POST['topic'], $_POST['score'], $_POST['user'])) {
+        $difficulty = $_POST['difficulty'];
+        $topic = $_POST['topic'];
+        $score = intval($_POST['score']);
+        $user = $_POST['user'];
 
-    if(isset($_POST)) {
-        
-            $difficulty = $_POST['difficulty'];
-            $topic = $_POST['topic'];
-            $score = $_POST['score'];
-            $user = $_POST['user'];
-            echo "Your topic is " . $topic . " and your level was " . $difficulty . ". Your final score is " . $score . ". Well done, " . $user;
+        // Output for testing
+        echo "Your topic is " . $topic . " and your level was " . $difficulty . ". Your final score is " . $score . ". Well done, " . $user;
 
-            $query = "INSERT INTO playerscore (user, topic, difficulty, score)
-                                VALUES ('$user', '$topic', '$difficulty', '$score')";
+        // Insert data into the database (use prepared statements to prevent SQL injection)
+        $stmt = $con->prepare("INSERT INTO playerscore (user, topic, difficulty, score) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("sssi", $user, $topic, $difficulty, $score);
+        $stmt->execute();
+
+        // Close the statement
+        $stmt->close();
+
     } else {
-        echo "Error!";
+        echo "One or more required fields are missing.";
     }
+} else {
+    echo "Invalid request method.";
+}
+?>
--- src/components/ScoreForm.jsx
+++ src/components/ScoreForm.jsx
@@ -9,22 +9,13 @@ export default function ScoreForm() {
     let result;
     const submitForm = (e) => {
         e.preventDefault()
-        // const gameD = e.target[0].value
-        // const gameT = e.target[1].value
-        // const gameScore = e.target[2].value
-
-        // console.log(`${playerName} played the ${gameD} level for ${gameT} and acquired ${gameScore} points.`)
-        // setScoreFormActive(false)
-        // setPoints(0)
-        // setPlayerName("")
 
         const form = e.target
-        console.log(form.attributes[1].value)
+        console.log(e.target.action)
         const xhr = new XMLHttpRequest();
 
         xhr.open('POST', form.attributes[1].value);
         // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
 
         xhr.addEventListener('readystatechange', function () {
             if (this.readyState === this.DONE) {
@@ -36,6 +27,10 @@ export default function ScoreForm() {
         const formData = new FormData(form);
         xhr.send(formData);
 
+        setScoreFormActive(false)
+        setPoints(0)
+        setPlayerName("")
+
 
     }
 
@@ -45,7 +40,7 @@ export default function ScoreForm() {
         
             <h2>Great Work!</h2>    
             <p>Record your high score here:</p>
-            <form className="flex flex-col" action="http://localhost:8000/server.php" method="post" onSubmit={(e) => submitForm(e)}>
+            <form className="flex flex-col" action="http://localhost:8000/server.php" method="POST" onSubmit={(e) => submitForm(e)}>
 
                 {/* hide these readonly inputs and display their values in paragraphs for nicer UX */}
                 <input className="hidden" type="text" name="difficulty" value={gameDifficulty} readOnly />
@@ -57,11 +52,11 @@ export default function ScoreForm() {
                 <input className="hidden" type="number" name="score" value={points} readOnly />
                 <p>Final Score: {points}</p>
 
-                <input type="text" name="user" onChange={(e) => setPlayerName(!playerName && e.target.value)} placeholder="Enter name..." defaultValue={playerName} required />
+                <input type="text" name="user" onChange={(e) => setPlayerName(e.target.value)} placeholder="Enter name..." defaultValue={playerName} required />
+
 
                 <button type="submit" name="score_submit">Submit</button>
 
-                <h2>{result}</h2>
             </form>
 
         </>
